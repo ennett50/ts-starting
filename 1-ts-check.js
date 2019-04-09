f1c5e9db68862a6eaccfab1e158636b17adc0ef4
ennet <aperyakina@goodwix.team>
2019-04-09 12:31:41 +0300

    added js fail for example

---
+++ 1-ts-check.js
@@ -0,0 +1,35 @@
+Object.defineProperty(window, 'MySweetApp', {value: 'v1.0.0', readonly: false});
+
+
+function deliveryMethod() {
+    //TODO
+    return 'overnight';
+}
+
+function shipWeight() {
+    return parseInt((document.getElementById('weight')).innerHTML)
+}
+
+/**
+ *
+ * @param {string | string[]} email
+ */
+function setUpdates(email) {
+
+    function sendEmail(address) {
+
+        console.log(`Shipping to ${address} via ${deliveryMethod() | 'standart'} delivery`);
+
+        if (shipWeight() > 100) {
+            console.log('Warning: oversize package');
+        }
+    }
+
+    if (email.length) {
+        email.forEach((idx, val) => {
+            sendEmail(val.trim());
+        })
+    } else {
+        sendEmail(email.trim());
+    }
+}

a057b70be9b7071a23c7bffc86592281a33a9086
ennet <aperyakina@goodwix.team>
2019-07-24 11:44:26 +0300

    types in ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,67 @@
-console.log('Hello')
+// interface IAccount {
+//   firstName: string;
+//   age: number;
+// }
+//
+// let user = IAccount;
+
+
+interface IAccount {
+  firstName: string;
+  age: number;
+}
+
+let user = {
+  firstName: 'Игорь',
+  age: 33,
+};
+
+let person: typeof user = {
+  age: 33,
+  firstName: 'Got',
+};
+
+
+let age:number  = 1;
+// let age  = 1;
+
+// age = 'qwe';
+
+enum Size {
+  S,
+  M,
+  XL,
+}
+// enum Size {
+//   S = 44,
+//   M = 48,
+//   XL = 50,
+// }
+
+let num = Size.S;
+// let num = Size[44];
+
+enum Actions {
+  AddUser = 'ADD_USER',
+  RemoveUser = 'REMOVE_USER',
+
+  DeleteUser = RemoveUser, // alias
+}
+
+// const asction = Actions[Actions.AddUser] ошибка, надо учитывать верно
+
+
+function reducer(_state : number, action : {type: Actions}) : number {
+  switch (action.type) {
+    case Actions.AddUser: {
+      return 1;
+    }
+    case Actions.RemoveUser: {
+      return 1;
+    }
+
+    default: {
+      return 0;
+    }
+  }
+}

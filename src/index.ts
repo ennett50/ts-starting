22fdf20321dbdc83451159c937baf936108e240a
ennet <aperyakina@goodwix.team>
2019-07-31 16:30:13 +0300

    get and set types and declare

--- src/index.ts
+++ src/index.ts
@@ -66,48 +66,67 @@
 //   }
 // }
 
-let isDonae : boolean = false;
+// let isDonae : boolean = false;
+//
+// let decimal: number = 6;
+// let hex: number = 0xf00d;
+// let binary : number = 0b1010;
+// let octal: number = 0o744;
+// let nan: number = NaN;
+// let infinity: number = Infinity;
+//
+// let myName : string = 'Igor';
+//
+// let nullValue : null = null;
+// let undefinedValue : undefined = undefined;
+//
+// let bigNum : bigint = BigInt(100);
+// let bigNum2 : bigint = BigInt(100n);
+//
+// function fibanacci(n:bigint) {
+//   let result: bigint = 1n;
+//
+//   for (let last : bigint = 0n, i : bigint = 0n; i < n; i ++) {
+//     const current : bigint = result;
+//     result += last;
+//     last = current;
+//   }
+//   return result;
+// }
+//
+// const key1 : unique symbol = Symbol('key1');
+// const key2 : symbol = Symbol('key2');
+// const key3 : symbol = Symbol('key3');
+//
+// const strictObject : {[ key1: string] : number | string} = {
+//   [key1] : 100,
+//   [key2] : 'hi',
+// };
+//
+// let a1 = strictObject[key1];  // symbol не может использоваться для индекса => unique symbol
+//
+// function fn(value: typeof key1): void {
+//   // если не unique symbol, то можно передать все что угодно
+//   // заменяем
+// }
+//
+// fn(key1);
 
-let decimal: number = 6;
-let hex: number = 0xf00d;
-let binary : number = 0b1010;
-let octal: number = 0o744;
-let nan: number = NaN;
-let infinity: number = Infinity;
+// interface IAnimationsOptions {
+//   delayX: number;
+//   delayY: number;
+//   easing: 'ease-in' | 'ease-in-out' | 'ease-out';
+//
+// }
+//
+// // Такой класс есть физически, где-то объявлен и просто знай о нем
+// declare class UIElement {
+//   public animate(options: IAnimationsOptions): void;
+// }
+//
+// new UIElement().animate({ delayX: 10, delayY: 10, easing: 'ease-out' });
+//
+// type Digits = 0 | 1 | 2 | 3;
+//
+// let num: Digits = 0;
 
-let myName : string = 'Igor';
-
-let nullValue : null = null;
-let undefinedValue : undefined = undefined;
-
-let bigNum : bigint = BigInt(100);
-let bigNum2 : bigint = BigInt(100n);
-
-function fibanacci(n:bigint) {
-  let result: bigint = 1n;
-
-  for (let last : bigint = 0n, i : bigint = 0n; i < n; i ++) {
-    const current : bigint = result;
-    result += last;
-    last = current;
-  }
-  return result;
-}
-
-const key1 : unique symbol = Symbol('key1');
-const key2 : symbol = Symbol('key2');
-const key3 : symbol = Symbol('key3');
-
-const strictObject : {[ key1: string] : number | string} = {
-  [key1] : 100,
-  [key2] : 'hi',
-};
-
-let a1 = strictObject[key1];  // symbol не может использоваться для индекса => unique symbol
-
-function fn(value: typeof key1): void {
-  // если не unique symbol, то можно передать все что угодно
-  // заменяем
-}
-
-fn(key1);
